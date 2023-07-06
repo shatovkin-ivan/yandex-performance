4751f1ccbb03ae1afed74aef1cf1b1ee81055483
ivan <shatowkln.ivan@yandex.ru>
2023-07-06 03:03:51 +0300

    fix

--- vendors/main.js
+++ vendors/main.js
@@ -354,12 +354,10 @@ function Main() {
     </main>;
 }
 
-setTimeout(() => {
-    const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
     root.render(
         <>
             <Header />
             <Main />
         </>
     );
-}, 100);
